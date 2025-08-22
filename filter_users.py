8617e43d1d47819c3c67be339fc7a74aefbddf6c
moritzbrosch36 <moritz.brosch23@googlemail.com>
2025-08-22 13:53:07 +0200

    Adjustments according to PEP8.

--- filter_users.py
+++ filter_users.py
@@ -5,10 +5,13 @@ def filter_users_by_name(name):
     with open("users.json", "r") as file:
         users = json.load(file)
 
-    filtered_users = [user for user in users if user["name"].lower() == name.lower()]
+    filtered_users = [
+        user for user in users if user["name"].lower() == name.lower()
+    ]
 
     for user in filtered_users:
         print(user)
+
 
 def filter_users_by_age(age):
     with open("users.json", "r") as file:
@@ -24,14 +27,19 @@ def filter_users_by_email(email):
     with open("users.json", "r") as file:
         users = json.load(file)
 
-    filtered_users_email = [user for user in users if user.get("email", "").lower() == email.lower()]
+    filtered_users_email = [
+        user for user in users if user.get("email", "").lower() == email.lower()
+    ]
 
     for user in filtered_users_email:
         print(user)
 
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (name, age or email): ").strip().lower()
+    filter_option = (
+        input("What would you like to filter by? (name, age or email): ")
+        .strip().lower()
+    )
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
